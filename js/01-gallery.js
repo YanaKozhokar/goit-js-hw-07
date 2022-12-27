7d0ff18e32da51d75035bcfac6181c6a6b2af7c2
Yana Kozhokar <yanakozhokar1996@gmail.com>
2022-12-27 15:49:08 +0200

    task-01
    Added a function for creating gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,24 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryRef = document.querySelector(".gallery");
+
+function createGalleryMarkup(array) {
+  return array
+    .map(
+      (item) =>
+        `<div class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+            <img
+            class="gallery__image"
+            src="${item.preview}"
+            data-source="${item.original}"
+            alt="${item.description}"
+            />
+        </a>
+    </div>`
+    )
+    .join("");
+}
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+galleryRef.insertAdjacentHTML("beforeend", galleryMarkup);
